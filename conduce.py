056a7f6323473c6d919330d926d87be0e8ff3c21
Kevin Van Leer <kvl@conduce.com>
2016-12-13 08:52:23 -0600

    Create a dataset
    and optionally add data to it

--- conduce.py
+++ conduce.py
@@ -1,6 +1,7 @@
 import requests
 import session
 import config
+import json
 
 
 def list_orchestrations(args):
@@ -26,9 +27,27 @@ def list_saved(thing_to_list, args):
     return make_get_request("conduce/api/%s/saved" % thing_to_list, args)
 
 
-def make_get_request(uri, args):
-    import jsbeautifier
+def wait_for_job(job_id, args):
+    finished = False
 
+    while not finished:
+        time.sleep(0.5)
+        response = make_get_request('conduce/api/%s' % job_id)
+        if int(response.status_code / 100) != 2:
+            print "Error code %s: %s" % (response.status_code, response.text)
+            return False;
+
+        if response.ok:
+            msg = response.json()
+            if 'response' in msg:
+                print "Job completed successfully."
+                finished = True
+        else:
+            print resp, resp.content
+            break
+
+
+def make_get_request(uri, args):
     if args.host:
         host = args.host
     else:
@@ -43,16 +62,60 @@ def make_get_request(uri, args):
     else:
         auth = session.get_session(host, user)
 
+    url = 'https://%s/%s' % (host, uri)
     if 'Authorization' in auth:
-        response = requests.get('https://%s/%s' % (host, uri), headers=auth)
+        response = requests.get(url, headers=auth)
     else:
         response = requests.get(url, cookies=auth)
     response.raise_for_status()
-    return jsbeautifier.beautify(response.content)
+    return response
+
+
+def ingest_data(dataset_id, data, args):
+    response = make_post_request(data, 'conduce/api/datasets/add_datav2/%s' % dataset_id, args)
+    if 'location' in response.headers:
+        job = response.headers['location']
+        response = wait_for_job(job_id, args)
+    return response
+
+
+def create_dataset(args):
+    response = make_post_request({'name':args.name}, 'conduce/api/datasets/createv2', args)
+
+    if args.json:
+        response_dict = json.loads(response)
+        ingest_data(response_dict['dataset'], args.json, args)
+    elif args.csv:
+        print "TODO: Allow users to import directly from CSV"
+    return response
+
+
+def make_post_request(payload, uri, args):
+    if args.host:
+        host = args.host
+    else:
+        host = config['default-host']
+    if args.user:
+        user = args.user
+    else:
+        user = config['default-user']
+
+    if args.api_key:
+        auth = session.api_key_header(args.api_key)
+    else:
+        auth = session.get_session(host, user)
+
+    url = 'https://%s/%s' % (host, uri)
+    if 'Authorization' in auth:
+        response = requests.post(url, json=payload, headers=auth)
+    else:
+        response = requests.post(url, json=payload, cookies=auth)
+    response.raise_for_status()
+    return response
 
 
 if __name__ == '__main__':
-    import argparse
+    import argparse, jsbeautifier
 
     arg_parser = argparse.ArgumentParser(description='Conduce command line utility')
     subparsers = arg_parser.add_subparsers(help='help for subcommands')
@@ -103,10 +166,22 @@ if __name__ == '__main__':
     parser_config_list.add_argument('--api-key', help='The API key used to authenticate')
     parser_config_list.set_defaults(func=list_from_args)
 
+    parser_create_dataset = subparsers.add_parser('create-dataset', help='Create a Conduce dataset')
+    parser_create_dataset.add_argument('name', help='The name to be given to the new dataset')
+    parser_create_dataset.add_argument('--user', help='The user who owns the new dataset')
+    parser_create_dataset.add_argument('--host', help='The server on which the dataset will be created')
+    parser_create_dataset.add_argument('--api-key', help='The API key used to authenticate')
+    parser_create_dataset.add_argument('--json', help='Optional: A well formatted Conduce entities JSON file')
+    parser_create_dataset.add_argument('--csv', help='Optional: A CSV file that can be parsed as Conduce data')
+    parser_create_dataset.set_defaults(func=create_dataset)
+
     args = arg_parser.parse_args()
 
     config = config.get_full_config()
 
     result = args.func(args)
     if result:
-        print result
+        if hasattr(result, 'content'):
+            print jsbeautifier.beautify(result.content)
+        else:
+            print jsbeautifier.beautify(result)
