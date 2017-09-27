d8528cd1a6db4404438a5b00da4f4aba5d3a303c
Charles Law <claw@conduce.com>
2017-09-27 15:24:42 -0700

    Experiment with installing the requirements.txt with setup.py (even though it is not a great practice

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), "r") as f:
+    required = f.read().splitlines()
+
+
 
 setup(
     name="conduce",
-    version="2.0.1",
+    version="2.0.2",
     description="Python Distribution Utilities",
     author="Conduce",
     author_email="support@conduce.com",
     url="https://www.conduce.com",
+    install_requires=required,
     packages=["conduce"],
     package_dir={"conduce": ""},
     entry_points={
