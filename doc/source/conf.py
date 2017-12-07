ca6bc495936a4531894a1fefaa2132daaedc9d69
alex-conduce <alex@conduce.com>
2017-12-07 11:04:26 -0800

    now just using theme options to specify font

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -98,7 +98,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'font_family':'Gotham Rounded Light',
+        'head_font_family':'Gotham Bold',
+        'code_font_family':'Roboto Mono'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
