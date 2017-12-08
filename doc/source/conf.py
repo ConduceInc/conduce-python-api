1d6a5a393013c9346034a2a335c751c64ba608a1
alex-conduce <alex@conduce.com>
2017-12-08 10:41:57 -0800

    SKW-2183

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -55,7 +55,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Conduce Python API'
+project = u'Python API'
 copyright = u'2017, Conduce'
 author = u'Kevin Van Leer'
 
