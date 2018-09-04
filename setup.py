38690705f37494ed63fdaa1f78524a812c929cbd
Kevin Van Leer <kvl@conduce.com>
2018-09-04 13:14:52 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), "r") as f
 
 
 setup(
-        name="conduce",
-        version="2.4.1",
-        description="Conduce Python API",
-        author="Conduce",
-        author_email="support@conduce.com",
-        url="https://www.conduce.com",
-        install_requires=required,
-        packages=["conduce"],
-        package_dir={"conduce": ""},
-        entry_points={
+    name="conduce",
+    version="2.5.1",
+    description="Conduce Python API",
+    author="Conduce",
+    author_email="support@conduce.com",
+    url="https://www.conduce.com",
+    install_requires=required,
+    packages=["conduce"],
+    package_dir={"conduce": ""},
+    entry_points={
             "console_scripts": ["conduce-api=conduce.conduce:main"],
-            },
-        )
+    },
+)
