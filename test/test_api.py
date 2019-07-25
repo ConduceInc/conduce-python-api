0c23f7e63cd07bef45c050c0dc0828a2301a0e5d
Kevin Van Leer <kvl@conduce.com>
2019-07-25 07:55:56 -0500

    cleanup

--- test/test_api.py
+++ test/test_api.py
@@ -1,18 +1,18 @@
 import unittest
 import mock
-import json
-import requests
 
 from conduce import api
-from requests.exceptions import HTTPError
+
 
 class ResultMock:
     content = "{\"apikey\": \"fake json content\"}"
-	
+
+
 class CustomHTTPException:
     return_value = 409
     msg = "Not found"
-	
+
+
 class Test(unittest.TestCase):
     @mock.patch('conduce.api.make_post_request', return_value=ResultMock())
     def test_create_resource_mime_type_json(self, mock_make_post_request):
@@ -84,32 +84,31 @@ class Test(unittest.TestCase):
         mock_b64encode.assert_called_once_with(test_content)
         mock_make_post_request.assert_called_once_with(
             expected_resource_def, 'conduce/api/v2/resources', **test_kwargs)
- 
+
     @mock.patch('conduce.api.make_post_request', return_value=ResultMock())
     def test_find_resource(self, mock_make_post_request):
         test_resource_type = "test_resource_type"
-        test_resource_name = "test_resource_name"
         test_content = "test_content_valid_json"
         json_mime_type = "application/json"
         test_kwargs = {'kwarg1': "arg1_value"}
 
-        api.find_resource(type='test_resource_type', mime = json_mime_type, tag=None, content=test_content, **test_kwargs)
+        api.find_resource(type='test_resource_type', mime=json_mime_type, tag=None, content=test_content, **test_kwargs)
         expected_resource_def = {
-              'type': test_resource_type,
-              'mime': "application/json",
-            }
+            'type': test_resource_type,
+            'mime': "application/json",
+        }
         mock_make_post_request.assert_called_once_with(
-            expected_resource_def, 'conduce/api/v2/resources/searches?content=test_content_valid_json',id=None, tag=None, mime='application/json',  content=test_content,  name=None, regex=None, type='test_resource_type', **test_kwargs)
+            expected_resource_def, 'conduce/api/v2/resources/searches?content=test_content_valid_json', id=None, tag=None, mime='application/json', content=test_content, name=None, regex=None, type='test_resource_type', **test_kwargs)
 
     @mock.patch('conduce.api.make_get_request', return_value=ResultMock())
     def test_list_api_keys(self, mock_make_get_request):
         test_kwargs = {'kwarg1': "arg1_value"}
-          
+
         result = api.list_api_keys(**test_kwargs)
         mock_make_get_request.assert_called_once_with(
             'apikeys/list', **test_kwargs)
         self.assertEquals(result['apikey'], 'fake json content')
-		
+
     @mock.patch('conduce.api.make_post_request', return_value=ResultMock())
     def test_create_api_key(self, mock_make_post_request):
         test_kwargs = {'kwarg1': "arg1_value"}
@@ -117,7 +116,7 @@ class Test(unittest.TestCase):
         mock_make_post_request.assert_called_once_with(
             {"description": "Generated and used by conduce-python-api"}, 'apikeys/create', **test_kwargs)
         self.assertEquals(result, 'fake json content')
-		
+
     @mock.patch('conduce.api.make_post_request')
     def test_remove_api_key(self, mock_make_post_request):
         test_api_key = "Test_API"
@@ -133,8 +132,8 @@ class Test(unittest.TestCase):
         test_kwargs = {'kwarg1': "arg1_value"}
         api.account_exists(test_email, **test_kwargs)
         mock_make_post_request.assert_called_once_with(
-            {'email': 'email@conduce.com'}, 'user/public/email-available', **test_kwargs)	
-    
+            {'email': 'email@conduce.com'}, 'user/public/email-available', **test_kwargs)
+
     @mock.patch('conduce.api.make_post_request')
     def test_create_account(self, mock_make_post_request):
         test_email = "email@conduce.com"
@@ -142,12 +141,12 @@ class Test(unittest.TestCase):
         test_kwargs = {'kwarg1': "arg1_value"}
         api.create_account(test_name, test_email, **test_kwargs)
         expected_resource_def = {
-              'name': test_name,
-              'email': test_email              
-            }
+            'name': test_name,
+            'email': test_email
+        }
         mock_make_post_request.assert_called_once_with(
             expected_resource_def, 'admin/create-user', **test_kwargs)
-			
+
     @mock.patch('conduce.api.make_post_request')
     def test_create_team(self, mock_make_post_request):
         team_name = "Conduce"
@@ -155,7 +154,7 @@ class Test(unittest.TestCase):
         api.create_team(team_name, **test_kwargs)
         mock_make_post_request.assert_called_once_with(
             {'name': team_name}, 'team-create', **test_kwargs)
-		
+
     @mock.patch('conduce.api.make_get_request')
     def test_list_teams(self, mock_make_get_request):
         test_kwargs = {'kwarg1': "arg1_value"}
@@ -170,7 +169,7 @@ class Test(unittest.TestCase):
         test_kwargs = {'kwarg1': "arg1_value"}
         api.add_user_to_team(team_id, test_email, **test_kwargs)
         mock_make_post_request.assert_called_once_with(
-            {'invitee': {'email': test_email}}, 'team/{}/invite'.format(team_id), **test_kwargs)			
+            {'invitee': {'email': test_email}}, 'team/{}/invite'.format(team_id), **test_kwargs)
 
     @mock.patch('conduce.api.make_get_request')
     def test_list_team_members(self, mock_make_get_request):
@@ -205,7 +204,7 @@ class Test(unittest.TestCase):
         test_kwargs = {'kwarg1': "arg1_value"}
         api.add_user_to_group(team_id, test_group_id, test_user_id, **test_kwargs)
         mock_make_post_request.assert_called_once_with(
-            {'id': test_user_id}, 'team/{}/group/{}/add-user'.format(team_id, test_group_id), **test_kwargs)	
+            {'id': test_user_id}, 'team/{}/group/{}/add-user'.format(team_id, test_group_id), **test_kwargs)
 
     @mock.patch('conduce.api.make_get_request')
     def test_list_group_members(self, mock_make_get_request):
@@ -214,7 +213,7 @@ class Test(unittest.TestCase):
         test_kwargs = {'kwarg1': "arg1_value"}
         api.list_group_members(team_id, test_group_id, **test_kwargs)
         mock_make_get_request.assert_called_once_with(
-            'team/{}/group/{}/users'.format(team_id, test_group_id), **test_kwargs)	
+            'team/{}/group/{}/users'.format(team_id, test_group_id), **test_kwargs)
 
     @mock.patch('conduce.api.make_post_request')
     def test_set_owner(self, mock_make_post_request):
@@ -223,7 +222,7 @@ class Test(unittest.TestCase):
         test_kwargs = {'kwarg1': "arg1_value"}
         api.set_owner(team_id, test_resource_id, **test_kwargs)
         mock_make_post_request.assert_called_once_with(
-            {}, 'acl/{}/transfer-to-team/{}'.format(test_resource_id, team_id), **test_kwargs)		
+            {}, 'acl/{}/transfer-to-team/{}'.format(test_resource_id, team_id), **test_kwargs)
 
     @mock.patch('conduce.api.make_get_request')
     def test_list_permissions(self, mock_make_get_request):
@@ -284,22 +283,23 @@ class Test(unittest.TestCase):
             'target': test_target_string
         }
         mock_make_post_request.assert_called_once_with(
-            expected_resource_def, 'acl/{}/revoke'.format(test_resource_id), **test_kwargs)	
+            expected_resource_def, 'acl/{}/revoke'.format(test_resource_id), **test_kwargs)
 
     @mock.patch('conduce.api.make_put_request', return_value=ResultMock())
     def test_update_resource(self, mock_make_put_request):
         test_resource_def = {
-            'revision': 1, 
-            'id' : "resource_id_123"
+            'revision': 1,
+            'id': "resource_id_123"
         }
         test_kwargs = {'kwarg1': "arg1_value"}
-        res = api.update_resource(test_resource_def, **test_kwargs)
+        api.update_resource(test_resource_def, **test_kwargs)
         expected_resource_def = {
-            'revision': 2, 
-            'id' : "resource_id_123"
+            'revision': 2,
+            'id': "resource_id_123"
         }
         mock_make_put_request.assert_called_once_with(
             expected_resource_def, 'conduce/api/v2/resources/{}'.format(test_resource_def['id']), **test_kwargs)
-			
+
+
 if __name__ == '__main__':
     unittest.main()
